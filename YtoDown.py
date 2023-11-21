2e164e7df833441fbca0d355e43935cca2b9ef95
Johngtka <johngtka@gmail.com>
2023-11-21 12:37:31 +0100

    feat: added downloading video from yt and convert it it mp3

--- YtoDown.py
+++ YtoDown.py
@@ -1,38 +1,70 @@
 import os
 from pytube import YouTube
+from moviepy.editor import VideoFileClip
+from shutil import move, rmtree
 
 
-def download_audio_youtube(url, output_path):
+def download_and_convert_video(url, output_path):
     try:
-        # Pobierz strumień audio z YouTube
+        # Download the YouTube video as an MP4 file
         youtube = YouTube(url)
-        audio_stream = youtube.streams.filter(only_audio=True).first()
+        video_stream = youtube.streams.filter(file_extension='mp4').first()
 
-        # Sprawdź, czy katalog docelowy istnieje, jeśli nie, utwórz go
+        # Check if the output directory exists, if not, create it
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
-        # Pobierz audio jako plik webm
-        mp3_file = os.path.join(output_path, f"{audio_stream.title}.mp3")
-        audio_stream.download(output_path, mp3_file)
+        # Download video as an MP4 file
+        video_file = os.path.join(output_path, f"{video_stream.title}.mp4")
+        video_stream.download(output_path, video_file)
 
-        print(f"Pobrano audio jako plik MP3: {mp3_file}")
+        print(f"Downloaded video as MP4: {video_file}")
+
+        # Convert MP4 to MP3
+        mp3_file = os.path.join(output_path, f"{video_stream.title}.mp3")
+        clip = VideoFileClip(video_file)
+        clip.audio.write_audiofile(mp3_file)
+        clip.close()
+
+        print(f"Converted video to MP3: {mp3_file}")
         return mp3_file
+
     except Exception as e:
-        print(f"Wystąpił błąd podczas pobierania: {e}")
+        print(f"An error occurred: {e}")
         return None
 
 
+def move_to_downloads_and_cleanup(mp3_file, output_path):
+    try:
+        # Move the MP3 file to the user's "Downloads" folder
+        downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+        downloads_mp3_file = os.path.join(
+            downloads_path, os.path.basename(mp3_file))
+        move(mp3_file, downloads_mp3_file)
+
+        print(f"Moved MP3 file to Downloads: {downloads_mp3_file}")
+
+        # Remove the MP4 file and the output folder
+        os.remove(os.path.join(output_path, f"{
+                  os.path.splitext(os.path.basename(mp3_file))[0]}.mp4"))
+        rmtree(output_path)
+
+        print("Cleanup complete")
+
+    except Exception as e:
+        print(f"An error occurred during cleanup: {e}")
+
+
 if __name__ == "__main__":
-    # Pobierz katalog bieżący (folder, w którym znajduje się plik programu)
+    # Get the current folder (the folder where the script is located)
     current_folder = os.path.dirname(__file__)
 
-    youtube_url = input("Podaj URL do filmu: ")
+    youtube_url = input("Enter the YouTube video URL: ")
 
-    # Ustaw ścieżkę do folderu, gdzie mają być zapisane pliki
+    # Set the path to the folder where files will be saved
     output_folder = os.path.join(current_folder, "output")
 
-    mp3_file = download_audio_youtube(youtube_url, output_folder)
+    mp3_file = download_and_convert_video(youtube_url, output_folder)
 
     if mp3_file:
-        print(f"Plik MP3 dostępny pod ścieżką: {mp3_file}")
+        move_to_downloads_and_cleanup(mp3_file, output_folder)
