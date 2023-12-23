77ae0ab955d8ed730330ea3a7c6e39a1a9f45724
Johngtka <johngtka@gmail.com>
2023-12-23 10:43:16 +0100

    refactor: removed not needed comments

--- YtoDown.py
+++ YtoDown.py
@@ -8,7 +8,6 @@ from shutil import move, rmtree
 
 def download_video(url, output_path):
     try:
-        # Download the YouTube video as an MP4 file
         youtube = YouTube(url)
 
         # Check if the video is age restricted
@@ -26,12 +25,11 @@ def download_video(url, output_path):
         video_title = "".join(c if c.isalnum() or c in [
                               ' ', '.', '-', '_'] else '_' for c in video_stream.title)
 
-        # Download video as an MP4 file
         video_file = os.path.join(output_path, f"{video_title}.mp4")
         video_stream.download(output_path, video_file)
 
         print(f"Downloaded video as MP4: {video_file}")
-        time.sleep(2)  # Opóźnienie na potrzeby obserwacji
+        time.sleep(2)
 
         return video_file
 
@@ -42,7 +40,6 @@ def download_video(url, output_path):
 
 def convert_to_mp3(video_file, output_path):
     try:
-        # Convert MP4 to MP3
         print("Converting to MP3...")
         mp3_file = os.path.join(
             output_path, f"{os.path.splitext(os.path.basename(video_file))[0]}.mp3")
@@ -51,7 +48,7 @@ def convert_to_mp3(video_file, output_path):
         clip.close()
 
         print(f"Converted video to MP3: {mp3_file}")
-        time.sleep(2)  # Opóźnienie na potrzeby obserwacji
+        time.sleep(2)
 
         return mp3_file
 
@@ -69,7 +66,7 @@ def move_to_downloads(mp3_file):
         move(mp3_file, downloads_mp3_file)
 
         print(f"Moved MP3 file to Downloads: {downloads_mp3_file}")
-        time.sleep(2)  # Opóźnienie na potrzeby obserwacji
+        time.sleep(2)
 
     except Exception as e:
         print(f"An error occurred during file move: {e}")
@@ -79,11 +76,11 @@ def cleanup(output_path, mp3_file):
     try:
         # Remove the MP4 file and the output folder
         os.remove(os.path.join(output_path, f"{
-                  os.path.splitext(os.path.basename(mp3_file))[0]}.mp4"))
+            os.path.splitext(os.path.basename(mp3_file))[0]}.mp4"))
         rmtree(output_path)
 
         print("Cleanup done: Removed MP4 and output folder.")
-        time.sleep(2)  # Opóźnienie na potrzeby obserwacji
+        time.sleep(2)
 
     except Exception as e:
         print(f"An error occurred during cleanup: {e}")
@@ -92,21 +89,17 @@ def cleanup(output_path, mp3_file):
 if __name__ == "__main__":
     while True:
         if getattr(sys, 'frozen', False):
-            # Executable (.exe)
             script_directory = os.path.dirname(sys.executable)
         else:
-            # Script (.py)
             script_directory = os.path.dirname(os.path.abspath(__file__))
 
         output_folder = os.path.join(script_directory, "output")
 
-        # Input YouTube URL
         youtube_url = input("Enter the YouTube URL (or press Enter to exit): ")
 
         if not youtube_url:
             break
 
-        # Download and convert video
         video_file = download_video(youtube_url, output_folder)
 
         if video_file:
